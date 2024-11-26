0cfde4f6cc9566c0f8b8a1d997479767ff354855
dmitru4ok <yurii.dmytruk@mif.stud.vu.lt>
2024-11-26 19:55:14 +0200

    One vm call for getting template

--- telecomms/web/app.py
+++ telecomms/web/app.py
@@ -1,34 +1,24 @@
-#!/bin/python3
 from flask import Flask, render_template, redirect, url_for, request, session, flash
-from helpers import find_user_in_db
+from helpers import find_user_in_db, get_vm_data
+from onevm import get_nebula_oneadmin_templates
 
 app = Flask(__name__)
 app.secret_key = "Something secret!"
-users = [
-    {"login": "user1", "password": "passof1", "vm_ids": [1, 3]},
-    {"login": "user2", "password": "passof2", "vm_ids": [2]}
-]
 
 
-vms = [
-    {"id": 1, "open_nebula_id": 75526},
-    {"id": 2, "open_nebula_id": 72216},
-    {"id": 3, "open_nebula_id": 72214},
-]
 
 @app.route("/")
 def empty():
-    return redirect((url_for("/vms")))
+    return redirect((url_for("view_vm_list")))
 
 @app.route("/vms")
 def view_vm_list():
     if 'username' in session:
         username = session['username']
-        user_data = find_user_in_db(username, users) # should be substituted by db call
+        user_data = find_user_in_db(username)
         if user_data is not None:
-            users_vm_ids = user_data["vm_ids"]
-            users_vms_data = [vm for vm in vms if vm["id"] in users_vm_ids]
-            # call OpenNebela API/SHELL here to get info about vms
+            users_vms_data = get_vm_data(user_data["vm_ids"]) # query our db
+        
             return render_template('vms.html', user_data=user_data, vmlist=users_vms_data)
         
     return redirect(url_for('login'))
@@ -40,7 +30,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
         
-        user = find_user_in_db(username, users) # should be substituted by db call
+        user = find_user_in_db(username)
         if user is not None and user["password"] == password:
             session['username'] = username
             return redirect(url_for('view_vm_list'))
@@ -50,14 +40,31 @@ def login():
     
     return render_template('login.html')
 
+
 @app.route("/create", methods=['GET','POST'])
 def create():
     if 'username' in session:
         username = session['username']
-        user_data = find_user_in_db(username, users) # should be substituted by db call
-        return render_template("create.html", user_data=user_data)
+        user_data = find_user_in_db(username)
+        if user_data: 
+            templates = get_nebula_oneadmin_templates()
+            if request.method == 'GET':
+                return render_template("create.html", templates=templates, user_data=user_data)
+            
+            # CREATE VM LOGIC HERE
+            return redirect(url_for("view_vm_list"))
     
-    return redirect(url_for("login"))
+        return redirect(url_for("login"))
+
+
+    
+    
+
+
+@app.route('/vm/<int:vm_id>', methods=['GET', 'POST', 'DELETE'])
+def manage_vm(vm_id):
+    pass
+    
 
 
 @app.route('/logout')
--- telecomms/web/helpers.py
+++ telecomms/web/helpers.py
@@ -1,6 +1,32 @@
-def find_user_in_db(login, list):
-    for usr in list:
+from load_dotenv import load_dotenv
+from os import getenv
+
+
+
+
+# THIS SHOULD BE IN DB
+users = [
+    {"login": "user1", "password": "passof1", "vm_ids": [1, 3]},
+    {"login": "user2", "password": "passof2", "vm_ids": [2]}
+]
+
+
+vms = [
+    {"id": 1, "open_nebula_id": 75526},
+    {"id": 2, "open_nebula_id": 72216},
+    {"id": 3, "open_nebula_id": 72214},
+]
+
+# THESE SHOULD BE DB CALLS
+def find_user_in_db(login):
+    for usr in users:
         if usr["login"] == login:
             return usr
+
+    return None
+
+def get_vm_data(vm_ids: list[int]):
+    if vm_ids is not None:
+        return [vm for vm in vms if vm["id"] in vm_ids]
 
     return None
