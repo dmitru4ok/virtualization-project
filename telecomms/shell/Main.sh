2497c4ba090681f866d027ef3a87cfbee1083eb6
dmitru4ok <yurii.dmytruk@mif.stud.vu.lt>
2024-11-30 03:30:45 +0200

    add ansible_become_pass var, correct variable names

--- telecomms/shell/Main.sh
+++ telecomms/shell/Main.sh
@@ -47,12 +47,12 @@ DB_PRIVATE_IP=$(awk '/\[db\]/ {getline; print}' /etc/ansible/hosts)
 CLIENT_PRIVATE_IP=$(awk '/\[client\]/ {getline; print}' /etc/ansible/hosts)
 
 ansible-vault decrypt ../misc/credentials.yaml --vault-password-file vault-pass.txt
-echo "vm_pass: ${VM_PASS}" >> ../misc/credentials.yaml
+echo "ansible_become_pass: ${VM_PASS}" >> ../misc/credentials.yaml
 echo "db_vm_username: ${DB_VM_UNAME}" >> ../misc/credentials.yaml
 echo "db_ip: ${DB_PRIVATE_IP}" >> vault-pass.txt
-echo "webserver_vm_uname: ${WEBSERVER_VM_UNAME}" >> ../misc/credentials.yaml
+echo "webserver_vm_username: ${WEBSERVER_VM_UNAME}" >> ../misc/credentials.yaml
 echo "webserver_vm_pass: ${WEBSERVER_VM_PASS}" >> ../misc/credentials.yaml
-echo "client_vm_uname: ${CLIENT_VM_UNAME}" >> ../misc/credentials.yaml
+echo "client_vm_username: ${CLIENT_VM_UNAME}" >> ../misc/credentials.yaml
 ansible-vault encrypt ../misc/credentials.yaml --vault-password-file vault-pass.txt
 
 
